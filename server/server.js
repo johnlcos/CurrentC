f061f6531171503c18f152bd567040017fbf9b6f
weiwang0305 <weiwang0305@gmail.com>
2024-03-07 15:46:23 -0500

    added userRouter

--- server/server.js
+++ server/server.js
@@ -1,16 +1,29 @@
 const express = require('express');
-const { Pool } = require('pg')
-const app = express();
+const { Pool } = require('pg');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+
 const PORT = process.env.PORT || 8080;
+const app = express();
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
+import userRouter from './routers/userRouter';
 
 const pool = new Pool({
-  host:
-})
+  host: process.env.RDS_HOSTNAME,
+  user: process.env.RDS_USERNAME,
+  password: process.env.RDS_PASSWORD,
+  port: process.env.PORT,
+});
 
+app.use('/api/user', userRouter);
 
 app.get('/api/home', (req, res) => {
   res.json({ message: 'Hello World!' });

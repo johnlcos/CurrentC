8a6bf9abdc7323dd7bd0cc2e7149c413a48181c3
weiwang0305 <weiwang0305@gmail.com>
2024-03-08 13:45:52 -0500

    adjusted the data format

--- server/controllers/feedController.js
+++ server/controllers/feedController.js
@@ -6,7 +6,7 @@ feedController.getAllFeed = async (req, res, next) => {
   try {
     const queryStr = 'SELECT * from feeds';
     const result = await db.query(queryStr);
-    res.locals.results = result;
+    res.locals.results = result.rows;
     next();
   } catch (error) {
     next(error);

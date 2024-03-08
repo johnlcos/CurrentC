c358563e6a55ef677b3b54b29803d2ad24a4eba6
weiwang0305 <weiwang0305@gmail.com>
2024-03-08 15:11:26 -0500

    adjusted message wrapper to fixed height

--- client/components/message-wrapper.tsx
+++ client/components/message-wrapper.tsx
@@ -3,5 +3,5 @@ interface MessageWrapperProps {
 }
 
 export const MessageWrapper = ({ message }: MessageWrapperProps) => {
-  return <div className='text-[15px]'>{message}</div>;
+  return <div className='text-[15px] h-[120px] py-3'>{message}</div>;
 };

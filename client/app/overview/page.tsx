73d36ff78d626c8bf15a093b566c98ca59e7158e
John Costello <jlcos777@gmail.com>
2024-03-15 17:57:07 -0400

    adds new landing page, with routing and styling

---
+++ client/app/overview/page.tsx
@@ -0,0 +1,28 @@
+"use client";
+import { userInfo } from "os";
+import { SideNavBar } from "../side-navbar";
+import { MainFeed } from "@/components/main-feed";
+import { useEffect } from "react";
+
+export default function Home() {
+  useEffect(() => {
+    const getUserInfo = async () => {
+      const userInfoRes = await fetch("http://localhost:8080/auth/getUserInfo");
+      const userInfo = await userInfoRes.json();
+      console.log(userInfo);
+    };
+
+    getUserInfo();
+  }, []);
+
+  return (
+    <div id="main-container" className="h-full w-full flex">
+      <div className="w-1/5">
+        <SideNavBar />
+      </div>
+      <div className="w-4/5">
+        <MainFeed />
+      </div>
+    </div>
+  );
+}
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,27 +1,46 @@
-'use client';
-import { userInfo } from 'os';
-import { SideNavBar } from './side-navbar';
-import { MainFeed } from '@/components/main-feed';
-import { useEffect } from 'react';
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
-  useEffect(() => {
-    const getUserInfo = async () => {
-      const userInfoRes = await fetch('http://localhost:8080/auth/getUserInfo');
-      const userInfo = await userInfoRes.json();
-      console.log(userInfo);
-    };
-
-    getUserInfo();
-  }, []);
-
   return (
-    <div id='main-container' className='h-full w-full flex'>
-      <div className='w-1/5'>
-        <SideNavBar />
-      </div>
-      <div className='w-4/5'>
-        <MainFeed />
+    <div className="h-screen w-screen flex justify-center items-center">
+      <div className="flex flex-col items-center md:flex-row">
+        <Image src="/alerty.png" width={300} height={300} alt="Alerty Logo" />
+        <div className="flex flex-col gap-2">
+          <h2 className="text-center text-xl text-green-900 font-semibold">
+            Join Alerty!
+          </h2>
+          <p>Stay up to date with the latest financial news!</p>
+          <ul className="flex flex-col gap-2">
+            <li>
+              <Link
+                href="/"
+                className="block w-full text-center bg-green-700 p-3 rounded-lg text-gray-50 font-semibold shadow-md hover:bg-green-600 transition duration-300"
+              >
+                Sign up with Google
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/signup"
+                className="block w-full text-center bg-green-700 p-3 rounded-lg text-gray-50 font-semibold shadow-md hover:bg-green-600 transition duration-300"
+              >
+                Create Account
+              </Link>
+            </li>
+            <li>
+              <p className="text-center">Already have an account?</p>
+            </li>
+            <li>
+              <Link
+                href="/signin"
+                className="block w-full text-center bg-green-700 p-3 rounded-lg text-gray-50 font-semibold shadow-md hover:bg-green-600 transition duration-300"
+              >
+                Sign In
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );

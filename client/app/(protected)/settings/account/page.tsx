acb92d8c8c8f62c5db72860094770b94fef706f7
weiwang0305 <weiwang0305@gmail.com>
2024-03-29 12:14:20 -0400

    working on settings page

--- client/app/(protected)/settings/account/page.tsx
+++ client/app/(protected)/settings/account/page.tsx
@@ -1,9 +1,30 @@
+import { MdOutlineKeyboardArrowRight } from 'react-icons/md';
+
 const AccountSettingsPage = () => {
+  const settingsMenu = [
+    'Your account',
+    'Monetization',
+    'Premium',
+    'Creator Subscriptions',
+    'Security and account acceess',
+    'Privacy and safety',
+    'Notifications',
+    'Accessibility, display, and languages',
+    'Additional resources',
+    'Help center',
+  ];
   return (
-    <div className='text-text-white'>
+    <div className='text-text-white w-full h-[500px]'>
       <div>
-        Settings Page
-        <div>Settings</div>
+        <div className='mb-5 text-xl'>Settings</div>
+        {settingsMenu.map((setting) => (
+          <div key={setting} className='flex justify-between gap-y-2 h-10'>
+            <span>{setting}</span>
+            <span>
+              <MdOutlineKeyboardArrowRight />
+            </span>
+          </div>
+        ))}
       </div>
     </div>
   );

d4e1cd2bc7bf214f215cdfc744107dba744586cf
John Costello <jlcos777@gmail.com>
2024-04-18 14:31:54 -0400

    remove drizzle

--- server/dist/server.js
+++ server/dist/server.js
@@ -25,8 +25,8 @@ const app = (0, express_1.default)();
 const server = (0, node_http_1.createServer)(app);
 const io = new socket_io_1.Server(server, {
     cors: {
-        origin: "http://localhost:3000",
-        methods: ["GET", "POST"],
+        origin: 'http://localhost:3000',
+        methods: ['GET', 'POST'],
     },
 });
 app.use(body_parser_1.default.urlencoded({ extended: false }));
@@ -37,32 +37,35 @@ const feedRouter_1 = __importDefault(require("./routers/feedRouter"));
 const settingsRouter_1 = __importDefault(require("./routers/settingsRouter"));
 const authRouter_1 = __importDefault(require("./routers/authRouter"));
 const messageRouter_1 = __importDefault(require("./routers/messageRouter"));
-app.use("/auth", authRouter_1.default);
-app.use("/users", userRouter_1.default);
-app.use("/feed", feedRouter_1.default);
-app.use("/settings", settingsRouter_1.default);
-app.use("/messages", messageRouter_1.default);
-app.use("/overview", (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+app.use('/auth', authRouter_1.default);
+app.use('/users', userRouter_1.default);
+app.use('/feed', feedRouter_1.default);
+app.use('/settings', settingsRouter_1.default);
+app.use('/messages', messageRouter_1.default);
+app.use('/overview', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
     const { data, error } = yield supabase_1.default.auth.getSession();
     // console.log('session', data);
 }));
+app.get('/', (req, res) => {
+    res.status(200).json('working backend');
+});
 app.use((err, req, res, next) => {
     const errorObj = Object.assign({}, err);
     console.log(errorObj);
     return res.status(errorObj.status).json({ error: errorObj });
 });
-io.on("connection", (socket) => {
+io.on('connection', (socket) => {
     console.log(`a user connected ${socket.id}}`);
-    socket.on("join_room", (data) => {
+    socket.on('join_room', (data) => {
         const { chatId, user1, user2 } = data;
         socket.join(chatId);
         console.log(`user-${user1} joined room - ${chatId}`);
     });
-    socket.on("send_message", (data) => {
-        io.in(data.chat_id).emit("receive_message", data);
+    socket.on('send_message', (data) => {
+        io.in(data.chat_id).emit('receive_message', data);
     });
-    socket.on("disconnect", () => {
-        console.log("A user disconnected:", socket.id);
+    socket.on('disconnect', () => {
+        console.log('A user disconnected:', socket.id);
     });
 });
 server.listen(PORT, () => {

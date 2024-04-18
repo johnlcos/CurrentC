95c47f8bc21c29095bb55a75838f5869d7ac15d0
weiwang0305 <weiwang0305@gmail.com>
2024-04-18 17:35:23 -0400

    testing getMySecret

--- server/utils/supabase.ts
+++ server/utils/supabase.ts
@@ -1,11 +1,26 @@
 import { createClient } from '@supabase/supabase-js';
 import dotenv from 'dotenv';
+import AWS from 'aws-sdk';
 
 dotenv.config();
+const client = new AWS.SecretsManager({ region: 'us-east-2' });
 
-const supabase = createClient(
-  `${process.env.PROJECT_URL}`,
-  `${process.env.PROJECT_ANON_KEY}`
-);
+const getMySecret = async (SecretId: string) => {
+  const s = await client.getSecretValue({ SecretId }).promise();
+  return s.SecretString;
+};
+
+let supabase;
+
+const creatingClient = async () => {
+  const secret = await getMySecret(
+    'arn:aws:secretsmanager:us-east-2:525357733102:secret:currentC_access_secret-DPEGp6'
+  );
+  console.log(secret);
+  supabase = createClient(
+    `${process.env.PROJECT_URL}`,
+    `${process.env.PROJECT_ANON_KEY}`
+  );
+};
 
 export default supabase;

7fbd925ee6fa98022c40391405849fd1233a5009
John Costello <jlcos777@gmail.com>
2024-04-19 17:17:40 -0400

    refactor home page layout

--- client/app/(protected)/(main)/layout.tsx
+++ client/app/(protected)/(main)/layout.tsx
@@ -6,10 +6,20 @@ export default function Layout({
   children: React.ReactNode;
 }>) {
   return (
-    <div className="w-full h-full flex">
-      <div className="w-full pl-[16%] lg:pr-[35%] h-full">{children}</div>
-      <div className="w-2/6 p-0 fixed right-0 bg-surface hidden h-screen lg:block z-1">
-        <RightSideWrapper />
+    <div id="middle-right-inner" className="w-full h-full flex">
+      <div
+        id="middle-wrapper"
+        className="flex-grow lg:w-[500px] xl:w-[650px] h-full"
+      >
+        {children}
+      </div>
+      <div
+        id="right-wrapper"
+        className="w-[300px] xl:w-[400px] hidden lg:block"
+      >
+        <div className="w-[300px] xl:w-[400px] fixed">
+          <RightSideWrapper />
+        </div>
       </div>
     </div>
   );
